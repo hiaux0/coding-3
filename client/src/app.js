396b063b3b04d130621ec89373f01893d5adc67e
hdn <hieudn5293@yahoo.de>
2019-03-12 17:59:12 +0100

    (add) app: routes

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,47 @@
-import {PLATFORM} from 'aurelia-pal';
+import { PLATFORM } from 'aurelia-pal';
+import './app.less';
 
+
+/**
+ * - Configure Router
+ */
 export class App {
   constructor() {
     this.message = 'Hello World!';
+  }
+
+  configureRouter(config, router) {
+    config.map([
+      {
+        name: 'home',
+        route: '',
+        moduleId: PLATFORM.moduleName('./pages/home/home')
+      },
+      {
+        route: 'links',
+        moduleId: PLATFORM.moduleName('./pages/links/links'),
+        nav: true,
+        title: 'Links'
+      },
+      {
+        route: 'apps',
+        moduleId: PLATFORM.moduleName('./pages/apps/apps'),
+        nav: true,
+        title: 'Apps'
+      },
+      {
+        route: 'sandbox',
+        moduleId: PLATFORM.moduleName('./pages/sandbox/sandbox'),
+        nav: true,
+        title: 'sandbox'
+      },
+      {
+        route: 'uilib',
+        moduleId: PLATFORM.moduleName('./pages/uilib/uilib'),
+        nav: true,
+        title: 'uilib'
+      },
+    ]);
+    this.router = router;
   }
 }
