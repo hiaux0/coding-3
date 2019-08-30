d5289eda9a81151e9fd60667e694e29e9c5a3cd6
hdn <hieudn5293@yahoo.de>
2019-08-31 00:05:24 +0200

    lint(*)

--- client/src/pages/sandbox/sandbox.js
+++ client/src/pages/sandbox/sandbox.js
@@ -1,7 +1,9 @@
-import {bindable} from 'aurelia-framework';
-import './sandbox.less'
+//@ts-check
+
+import { bindable } from 'aurelia-framework';
+import './sandbox.less';
+
 
 export class Sandbox {
   @bindable value = 'Sandbox';
-
 }
