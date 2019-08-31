c135dfcdf5a0bcc7c770d98af3f93efb932b490b
hdn <hieudn5293@yahoo.de>
2019-08-31 23:28:20 +0200

    feat(commandPalette): Command palette ala vscode

---
+++ client/src/resources/attributes/autocomplete.js
@@ -0,0 +1,77 @@
+import { bindable, inject } from 'aurelia-framework';
+import { debounce } from 'lodash-decorators';
+
+@inject(Element)
+export class AutocompleteCustomAttribute {
+  @bindable list;
+  @bindable value;
+  @bindable suggestedList;
+  @bindable key;
+
+  /**
+   * As we allow different types in a `list`, this variable provides the key for the actual content in an Object.
+   * @param {String}
+   */
+  @bindable key;
+  /**
+   * The complete list you want to search in, accepts: ["", "",...], [Object, Object,...]
+   * @param {Array}
+   */
+  @bindable list;
+  /**
+   * After filtration, the remaining results.
+   * @param {Array}
+   */
+  @bindable suggestList;
+  /**
+   * The current value/symbol in the input field.
+   * @param {String}
+   */
+  @bindable value;
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  attached() {
+    this.preparedList = this.prepareList();
+  }
+
+  /**
+   * Allow the list, which should be autocomplete, to be as flexibel as possible.
+   */
+  prepareList() {
+    if (!this.list[0][this.key]) throw new Error('Key not present');
+
+    let key = this.key || '';
+    // if (!typeof this.list[0] !== 'object') return;
+
+    return this.list.map((listItem) => ({
+      name: listItem[key],
+      id: listItem.id,
+    }));
+  }
+
+  @debounce(66)
+  valueChanged(newValue, oldValue) {
+    let ignoreCase = newValue.toLowerCase();
+    this.suggestedList = this.filterByUserInput(ignoreCase);
+  }
+
+  suggestedListChanged(newValue) {
+  }
+
+  filterByUserInput(value) {
+    if (!this.preparedList) return;
+
+    let fileredList = this.preparedList.filter((listItem) => {
+      let ignoreCase = listItem.name.toLowerCase();
+      return ignoreCase.includes(value);
+    });
+    return fileredList;
+  }
+
+  detached() {
+    this.suggestedList = this.list;
+  }
+}
---
+++ client/src/resources/commandStorage/command-storage.js
@@ -0,0 +1,35 @@
+export const commandList = {
+  simpleCommand: [
+    {
+      name: 'Change to dark theme',
+      id: 'changeToDarkTheme',
+      origin: 'app.js darkTheme()'
+    },
+    {
+      name: 'Change to light theme',
+      id: 'changeToLightTheme',
+      origin: 'app.js lightTheme()'
+    },
+    {
+      name: 'Activate jumpable',
+      id: 'jumpable',
+      origin: 'common/jumpable'
+    },
+    {
+      name: 'Toggle Persist jumpable',
+      id: 'togglePersistJumpable',
+      origin: 'common/jumpable'
+    },
+    {
+      name: 'Toggle Mouse Coordinates',
+      id: 'toggleMouseCoords',
+      origin: 'app.js'
+    },
+    // Marked
+    {
+      name: 'Marked: Convert to html',
+      id: 'marked_convertToHtml',
+      origin: 'markdown-parser'
+    }
+  ]
+};
