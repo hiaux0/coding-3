a422f0d5af30b98b68d921b8c4f1563f11f37e92
hdn <hieudn5293@yahoo.de>
2019-08-31 23:29:59 +0200

    feat(*): various copy paste from coding-2: - segmented button - connect-list - zooming - sortable - tooltip

---
+++ client/src/resources/attributes/sortable.js
@@ -0,0 +1,59 @@
+import { bindable, inject } from 'aurelia-framework';
+import Sortable from 'sortablejs';
+
+@inject(Element)
+export class SortableCustomAttribute {
+  @bindable active = true;
+  @bindable sortableContext;
+  @bindable draggableName;
+  @bindable ordered = false;
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  get context() {
+    // Handle mixed case
+    let listAnchor =
+      this.element.getElementsByTagName(this.sortableContext)
+      || this.element.getElementsByTagName('ul')
+      || this.element.getElementsByTagName('ol');
+    return listAnchor || this.element;
+  }
+
+  attached() {
+    this.initSortable();
+  }
+
+  activeChanged() {
+    // Make sure elements are rendered
+    setTimeout(() => {
+      this.initSortable();
+    }, 0)
+  }
+
+  initSortable() {
+    if (!this.active) return;
+
+    // Make multiple lists draggable
+    if (this.context.constructor.name === "HTMLCollection") {
+      for (let listAnchor of this.context) {
+        this.sortableInstance = new Sortable(listAnchor, {
+          group: '123',
+          draggable: this.draggableName || 'li',
+          animation: 100,
+        });
+      }
+      return;
+    }
+
+    this.sortableInstance = new Sortable(this.context, {
+      group: '123',
+      draggable: 'li',
+      animation: 100,
+    });
+
+  }
+
+}
+
---
+++ client/src/resources/attributes/tooltip.js
@@ -0,0 +1,91 @@
+import { bindable, inject } from 'aurelia-framework';
+import '../common/styles/tooltip.less';
+
+@inject(Element)
+export class TooltipCustomAttribute {
+  @bindable({ primaryProperty: true }) text;
+  @bindable onAppear;
+
+  tooltip;
+
+  /**
+   * @type {boolean}
+   */
+  firstHover = true;
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  attached() {
+    // const allowed = ['더해줄', '생기고', '아이가'];
+    // if (!allowed.includes(this.element.innerText.trim())) return;
+
+    const parent = this.element.parentNode;
+    const textContent = this.element.textContent.trim();
+    this.tooltip = this.createTooltipElement(textContent);
+    parent.insertBefore(this.tooltip, this.element.nextSibling);
+    this.addTooltipEvents(this.element);
+  }
+
+  detached() {
+    this.removeTooltipEvents(this.element);
+    this.element.parentNode.removeChild(this.tooltip);
+  }
+
+  addTooltipEvents(element) {
+    element.addEventListener('mouseover', this.showTooltip);
+    element.addEventListener('mouseout', this.hideTooltip);
+  }
+
+  removeTooltipEvents(element) {
+    element.removeEventListener('mouseover', this.showTooltip);
+    element.removeEventListener('mouseout', this.hideTooltip);
+  }
+
+  /**
+   * On first hover, update text if appear callback is given
+   * Shows and positions the tooltip.
+   */
+  showTooltip = () => {
+    if (document.getSelection().toString() !== '') return;
+
+    if (this.firstHover && this.onAppear) {
+      const contextText = this.element.textContent.trim();
+      this.onAppear(contextText).then(res => {
+        const { translation } = res;
+
+        this.tooltip.innerText = translation;
+        this.firstHover = false;
+      });
+    }
+
+    this.tooltip.style.display = 'block';
+    this.tooltip.style.opacity = 1;
+    const { left } = this.setTooltipPosition(this.element);
+    this.tooltip.style.left = `${left}px`;
+  }
+
+  hideTooltip = () => {
+    this.tooltip.style.opacity = 0;
+    this.tooltip.style.display = 'none';
+  }
+
+  createTooltipElement(textContent) {
+    const tooltip = document.createElement('span');
+    tooltip.classList.add('tooltip');
+    tooltip.innerText = this.text || textContent;
+
+    return tooltip;
+  }
+
+  setTooltipPosition(element) {
+    const rect = element.getBoundingClientRect();
+    const xPos = rect.x + rect.width / 2 - 25;
+
+    return {
+      left: xPos
+    };
+  }
+}
+
