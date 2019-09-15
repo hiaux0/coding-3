1cc7db9a668d342c72676d62e710b65311fc73f5
hdn <hieudn5293@yahoo.de>
2019-09-16 00:23:27 +0200

    lvl(prisma): Step 2: Change Datamodel 2.6 Query relational data
    https://www.prisma.io/docs/get-started/02-change-data-model-TYPESCRIPT-t101/

--- server/prisma/leveling/prisma-server.ts
+++ server/prisma/leveling/prisma-server.ts
@@ -1,51 +1,63 @@
+// import { prisma } from './generated/prisma-client'
+
+// // A `main` function so that we can use async/await
+// async function main() {
+//   // Create a new user with a new post
+//   const newUser = await prisma.createUser({
+//     name: 'Bob',
+//     email: 'bob@prisma.io',
+//     posts: {
+//       create: [
+//         {
+//           title: 'Join us for GraphQL Conf in 2019',
+//         },
+//         {
+//           title: 'Subscribe to GraphQL Weekly for GraphQL news',
+//         },
+//       ],
+//     },
+//   })
+//   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+
+//   // Fetch single user
+//   const user = await prisma.user({ id: 'ck0lgchr9000k07377yvt6ca7' })
+
+//   // Filter user list
+//   const usersCalledAlice = await prisma.users({
+//     where: {
+//       name: 'Alice',
+//     },
+//   })
+
+//   // Update a user's name
+//   const updatedUser = await prisma.updateUser({
+//     where: { id: 'ck0lgchr9000k07377yvt6ca7' },
+//     data: { name: 'Bob' },
+//   })
+
+//   // Delete user
+//   // const deletedUser = await prisma.deleteUser({ id: 'ck0lgchr9000k07377yvt6ca7' })
+
+//   // Read all users from the database and print them to the console
+//   const allUsers = await prisma.users()
+//   console.log(allUsers)
+
+//   const allPosts = await prisma.posts()
+//   console.log(allPosts)
+// }
+
+// main().catch(e => console.error(e))
+
 import { prisma } from './generated/prisma-client'
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user with a new post
-  const newUser = await prisma.createUser({
-    name: 'Bob',
-    email: 'bob@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join us for GraphQL Conf in 2019',
-        },
-        {
-          title: 'Subscribe to GraphQL Weekly for GraphQL news',
-        },
-      ],
-    },
-  })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
-
-  // Fetch single user
-  const user = await prisma.user({ id: 'ck0lgchr9000k07377yvt6ca7' })
-
-  // Filter user list
-  const usersCalledAlice = await prisma.users({
-    where: {
-      name: 'Alice',
-    },
-  })
-
-  // Update a user's name
-  const updatedUser = await prisma.updateUser({
-    where: { id: 'ck0lgchr9000k07377yvt6ca7' },
-    data: { name: 'Bob' },
-  })
-
-  // Delete user
-  // const deletedUser = await prisma.deleteUser({ id: 'ck0lgchr9000k07377yvt6ca7' })
-
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log(allUsers)
-
-  const allPosts = await prisma.posts()
-  console.log(allPosts)
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma.user({ email: 'bob@prisma.io' }).posts()
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
 }
 
 main().catch(e => console.error(e))
+
 
 
