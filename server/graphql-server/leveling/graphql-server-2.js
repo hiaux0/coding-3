f5cb3be06f05a5b20d1d0577d03f1b80da0d9c77
hdn <hieudn5293@yahoo.de>
2019-09-15 16:19:17 +0200

    lvl(graphql): mutations

--- server/graphql-server/leveling/graphql-server-2.js
+++ server/graphql-server/leveling/graphql-server-2.js
@@ -6,6 +6,9 @@ const schema = buildSchema(`
         course(id: Int!): Course
         courses(topic: String): [Course]
     },
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    }
     type Course {
         id: Int
         title: String
@@ -59,10 +62,21 @@ const getCourses = function (args) {
   }
 }
 
+const updateCourseTopic = function ({ id, topic }) {
+  coursesData.map(course => {
+    if (course.id === id) {
+      course.topic = topic;
+      return course;
+    }
+  });
+  return coursesData.filter(course => course.id === id)[0];
+}
+
 const root = {
   course: getCourse,
-  courses: getCourses
-};// Create an express server and a GraphQL endpoint
+  courses: getCourses,
+  updateCourseTopic: updateCourseTopic
+}; // Create an express server and a GraphQL endpoint
 
 const app = express();
 app.use('/graphql', express_graphql({
