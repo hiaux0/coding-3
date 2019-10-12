ded8a6d6e2e1d45e6fe03cd7506fd7f71f396c70
hdn <hieudn5293@yahoo.de>
2019-10-12 19:03:22 +0200

    ➕(uilib): dynamic component routing

--- client/src/pages/uilib/uilib.js
+++ client/src/pages/uilib/uilib.js
@@ -1,7 +1,21 @@
-import {bindable} from 'aurelia-framework';
-import './uilib.less'
+//@ts-check
+
+import { bindable, PLATFORM } from 'aurelia-framework';
+import './uilib.less';
+
 
 export class Uilib {
   @bindable value = 'Uilib';
 
+  configureRouter(config, router) {
+    config.map([
+      {
+        name: 'uilibWelcome',
+        route: '',
+        moduleId: PLATFORM.moduleName('./uilib-welcome/uilib-welcome'),
+        nav: true
+      },
+    ]);
+    this.router = router;
+  }
 }
